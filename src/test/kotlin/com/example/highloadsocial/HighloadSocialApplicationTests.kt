18227e04e5263b91796a61c27d71227dea228567
Sergey Gerasimov <fatemancer@yandex.ru>
2023-01-02 17:42:31 +0300

    Initial commit

---
+++ src/test/kotlin/com/example/highloadsocial/HighloadSocialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.highloadsocial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HighloadSocialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
