40b1cabec6b9212f360a3001dad7d4fb72459c8e
Sergey Gerasimov <fatemancer@yandex.ru>
2023-01-02 17:46:30 +0300

    Added generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
+    id("org.openapi.generator") version "6.2.1"
 }
 
-group = "com.example"
+group = "info.hauu"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
@@ -25,6 +26,25 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.openApiGenerate {
+    dependsOn(":clean")
+}
+
+tasks.compileKotlin {
+    dependsOn (":openApiGenerate")
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    library.set("spring-boot")
+    apiPackage.set("org.openapi.api")
+    modelPackage.set("org.openapi.model")
+    invokerPackage.set("info.hauu.highloadsocial")
+    inputSpec.set("$rootDir/src/main/resources/openapi.json")
+    outputDir.set("$rootDir/generated/src")
+    additionalProperties.put("gradleBuildFile", false)
 }
 
 tasks.withType<KotlinCompile> {
